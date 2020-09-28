542abb919ad04435d5fff7098956d445cbdab5e5
jianzhen.yin <jianzhen.yin@tongdun.net>
2020-09-28 15:43:00 +0800

    add comment

--- demo/src/main/java/com/algorithm/Solution_19.java
+++ demo/src/main/java/com/algorithm/Solution_19.java
@@ -1,6 +1,20 @@
 package com.algorithm;
 
 /**
+ * Given a linked list, remove the n-th node from the end of list and return its head.
+ *
+ * Example:
+ *
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ *
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * Note:
+ *
+ * Given n will always be valid.
+ *
+ * Follow up:
+ *
+ * Could you do this in one pass?
  * @author jianzhen.yin
  * @date 2020/9/27
  */
